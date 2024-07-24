63d8a538da29019fb23d26b67615064b21e2360c
alderjunio <alder.junio@gmail.com>
2024-07-23 20:37:40 -0400

    update .js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,18 @@
 
 const pokeapi = {}
 
-pokeapi.getPokemons = (offset = 0, limit = 10) => {  
+pokeapi.getPokemonDetail = (pokemon) => {
+   return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeapi.getPokemons = (offset = 0, limit = 5) => {  
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+    .then((pokemons) => pokemons.map(pokeapi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonDetails) => pokemonDetails)
 }
+
