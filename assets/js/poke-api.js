dceeaab55b94ec33e87f98baabcf4973679f6e1f
alderjunio <alder.junio@gmail.com>
2024-07-24 12:34:05 -0400

    update arquivos JS

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,27 @@
 
 const pokeapi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail){
+  const pokemon = new Pokemon()
+  pokemon.number = pokeDetail.order
+  pokemon.name = pokeDetail.name
+  
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+  const [type] = types
+  
+  pokemon.types = types
+  pokemon. type = type
+
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+  return pokemon
+
+}
+
 pokeapi.getPokemonDetail = (pokemon) => {
-   return fetch(pokemon.url).then((response) => response.json())
+   return fetch(pokemon.url)
+          .then((response) => response.json())
+          .then(convertPokeApiDetailToPokemon)
 }
 
 pokeapi.getPokemons = (offset = 0, limit = 5) => {  
@@ -13,6 +32,6 @@ pokeapi.getPokemons = (offset = 0, limit = 5) => {
     .then((jsonBody) => jsonBody.results)
     .then((pokemons) => pokemons.map(pokeapi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonDetails) => pokemonDetails)
+    .then((pokemonDetails) => pokemonDetails) 
 }
 
