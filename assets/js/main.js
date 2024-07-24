5d404b5f186251ad40bea0935981297ff7a00680
alderjunio <alder.junio@gmail.com>
2024-07-24 13:41:32 -0400

    update js e css

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,17 @@
 
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon" ${pokemon.type}>
+    <li class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                     </ol>
 
-                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    <img src="${pokemon.photo}" 
+                         alt="${pokemon.name}">
                 </div>              
             </li>
     `
